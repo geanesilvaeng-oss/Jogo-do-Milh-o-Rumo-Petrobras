8abbde0de2b28db6e8c34d5caf5735e565be9c95
geanesilvaeng-oss <geanesilvaeng@gmail.com>
2026-07-29 19:55:05 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,35 @@
+const CACHE_NAME = 'milhao-petrobras-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './perguntas.js',
+  './manifest.json'
+];
+
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('milhao-petrobras-v1').then((cache) => {
-      return cache.addAll([
-        './index.html',
-        './style.css',
-        './script.js',
-        './perguntas.js'
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.claimClients();
 });
 
 self.addEventListener('fetch', (e) => {
